fe567574968e4b2f76f04cd1862ae31620ac1117
jcgonzalezb <jcgonzalezb@gmail.com>
2021-09-07 17:08:11 -0500

    Inventing is a combination of brains and materials

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+for i in range(1, 89):
+    if i in range(10, 12):
+        continue
+    if i in range(20, 23):
+        continue
+    if i in range(30, 34):
+        continue     
+    if i in range(40, 45):
+        continue
+    if i in range(50, 56):
+        continue
+    if i in range(60, 67):
+        continue
+    if i in range(70, 78):
+        continue
+    if i in range(80, 89):
+        continue
+    print("{:02d}".format(i), end=', ')
+print(89)
