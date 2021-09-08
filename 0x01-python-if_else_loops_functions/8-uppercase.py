9711f0563f4c7bc8fade6d6b3061b47277caed3c
jcgonzalezb <jcgonzalezb@gmail.com>
2021-09-07 20:24:50 -0500

    To uppercase

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -3,7 +3,7 @@ def uppercase(str):
     new_string = ""
     i = 0
     for i in str:
-        if ord(i) >= 97 and ord(i) < 123:
+        if ord(i) >= 97 and ord(i) < 122:
             new_string += chr(ord(i)-32)
         else:
             new_string += i
