a1a9c1b45a4e3280cccac1ef773b96fd9462c838
jcgonzalezb <jcgonzalezb@gmail.com>
2021-09-08 23:39:07 -0500

    Who are you?

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
+import hidden_4 as hidden
 if __name__ == "__main__":
-    import hidden_4
-    for i in dir(hidden_4):
-        if not i.startswith("__"):
-            print("{:s}".format(i))
+
+    for name in dir(hidden):
+        if not name.startswith('__'):
+            print(name)
