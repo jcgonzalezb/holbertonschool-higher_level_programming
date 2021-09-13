014ecf23800e69222b3f22af47d33cbd79633399
jcgonzalezb <jcgonzalezb@gmail.com>
2021-09-13 18:18:11 -0500

    Can you C me now?

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,8 +1,3 @@
 #!/usr/bin/env python3
 def no_c(my_string):
-    str1 = []
-    for i in range(0, len(my_string)):
-        if my_string[i] == 'c' or my_string[i] == 'C':
-            str1 = my_string[:i] + my_string[i+1:]
-
-    return str1
+    return ("".join(c for c in my_string if c not in "Cc"))
