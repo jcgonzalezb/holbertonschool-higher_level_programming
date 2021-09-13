415a4df2a1d845a353a51e9a01e2ae336be8e3cf
jcgonzalezb <jcgonzalezb@gmail.com>
2021-09-13 18:02:24 -0500

    Can you C me now?

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
 def no_c(my_string):
-    return "".join(i for i in my_string if i.lower() not in 'cC')
-
+    my_string = my_string.translate({ord('c'): None})
+    my_string = my_string.translate({ord('C'): None})
+    return my_string
