8cce9339fde55acc2747898752cd9f4cd9ce93c8
jcgonzalezb <jcgonzalezb@gmail.com>
2021-09-13 23:44:24 -0500

    Only by 2

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
+    values = []
     if my_list:
-        values = []
         for i in my_list:
-            if i % 2 is 0:
+            if i % 2 == 0:
                 values.append(True)
             else:
                 values.append(False)
