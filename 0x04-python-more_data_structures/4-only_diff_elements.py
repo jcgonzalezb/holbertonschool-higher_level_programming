ae32625908744ad8a95e6b8f0dcd13383706b46b
jcgonzalezb <jcgonzalezb@gmail.com>
2021-09-14 16:25:41 -0500

    Only differents

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -2,20 +2,9 @@
 def only_diff_elements(set_1, set_2):
     if set_1 or set_2 is not None:
         new_set = []
-        not_unique_list = []
-        final_list = []
-        for x in set_1:
-            for y in set_2:
-                if x == y:
-                    not_unique_list.append(x)
-
         set_1 = list(set_1)
-        set_2 = list(set_2)
-        new_set = set_1+set_2
-
-        for x in new_set:
-            for y in not_unique_list:
-                if x != y:
-                    final_list.append(x)
-
-        return final_list
+        set_2 = list(set_2)      
+        new_set1 = list(set(set_1)-set(set_2))
+        new_set2 = list(set(set_2)-set(set_1))
+        new_set = new_set1+new_set2
+        return new_set
