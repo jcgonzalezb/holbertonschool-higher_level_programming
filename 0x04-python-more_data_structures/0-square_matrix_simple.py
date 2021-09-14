263581e790e6dcc8bbba27707c6ddeeb3a166f38
jcgonzalezb <jcgonzalezb@gmail.com>
2021-09-14 12:14:08 -0500

    Squared simple

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -3,6 +3,6 @@ def square_matrix_simple(matrix=[]):
     if matrix is not None:
         new_m = []
         for rows in matrix:
-            new_m.append(list(map(lambda x: x**2, rows)
+            new_m.append(list(map(lambda x: x**2, rows)))
         return new_m
     return None
