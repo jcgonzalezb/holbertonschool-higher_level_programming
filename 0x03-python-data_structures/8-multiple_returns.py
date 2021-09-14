26634f2848ece7a329f0b37d53064b67767a9f2c
jcgonzalezb <jcgonzalezb@gmail.com>
2021-09-13 22:41:00 -0500

    More returns!

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -2,9 +2,9 @@
 def multiple_returns(sentence):
     lenght = len(sentence)
     first = sentence[0]
-    
+
     if sentence == 0:
         first = None
-    
+
     res = (lenght, first)
     return res
