98eb8abbdc24ac274114a81ebb983916ebb181dc
jcgonzalezb <jcgonzalezb@gmail.com>
2021-09-14 22:44:13 -0500

    Best score

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
     if a_dictionary is not None:
-        MaxKey = max(a_dictionary.items(), key = lambda x: x[1])[0]
+        MaxKey = max(a_dictionary.items(), key=lambda x: x[1])[0]
         return MaxKey
