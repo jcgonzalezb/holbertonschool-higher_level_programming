17093ed34918e886e4c4b161bd6091bd84a53bbf
jcgonzalezb <jcgonzalezb@gmail.com>
2021-09-27 12:38:56 -0500

    safe print list file

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    
+    try:
+        counter = 0
+        for i in my_list:
+            counter = counter + 1
+           
+        for i in range(0, x):
+            print('{:d}'.format(my_list[i]))
+        return x
+    except IndexError:
+        pass
+    finally:
+        if counter < x:
+            return counter
+        else:
+            return x
