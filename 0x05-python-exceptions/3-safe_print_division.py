df176e9b5f1aba138093727aa398ef9c5b5b3b9c
jcgonzalezb <jcgonzalezb@gmail.com>
2021-09-27 21:16:30 -0500

    safe_print_division update

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -5,11 +5,12 @@ def safe_print_division(a, b):
         f = 0
         c = a // b
         f = float(c)
-        print('Inside result: {:.1f}'.format(f), end='\n')
     except ZeroDivisionError:
-        print('Inside result: None', end='\n')
+        pass
     finally:
         if f > 0:
+            print('Inside result: {:.1f}'.format(f), end='\n')
             return f
         else:
+            print('Inside result: None', end='\n')
             return None
