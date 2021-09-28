ed10e759772a6a68844c00480435b8ddc33758f3
jcgonzalezb <jcgonzalezb@gmail.com>
2021-09-27 21:12:43 -0500

    safe_print_division

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def safe_print_division(a, b):
     try:
-        c = 0; f = 0
+        c = 0
+        f = 0
         c = a // b
         f = float(c)
         print('Inside result: {:.1f}'.format(f), end='\n')
