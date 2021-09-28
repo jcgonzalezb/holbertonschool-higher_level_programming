47ee8b0e135e98ada756dc14c5c658132be1dd4c
jcgonzalezb <jcgonzalezb@gmail.com>
2021-09-27 21:26:35 -0500

    safe_print_division update

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -3,7 +3,7 @@ def safe_print_division(a, b):
     try:
         c = 0
         f = 0
-        c = a // b
+        c = a / b
         f = float(c)
     except (ZeroDivisionError, TypeError):
         f = None
