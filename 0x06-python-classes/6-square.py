a4c686eadde140041a13fc2e44210f27450006d1
jcgonzalezb <jcgonzalezb@gmail.com>
2021-10-02 16:38:00 -0500

    pep8 correction

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -99,11 +99,12 @@ class Square:
         If size is less than 0, raise a ValueError exception
         with the message size must be >= 0.
         """
-        if type(value) is not tuple or len(value) is not 2 or \
-            type(value[0]) is not int or type(value[1]) is not int or \
-            value[0] or value[1] < 0:
-                raise TypeError("position must be a tuple of 2 positive integers")
-        self.__position = value
+        if type(value) is not tuple or len(value) != 2 or \
+           type(value[0]) is not int or type(value[1]) \
+           is not int or value[0] < 0 or value[1] < 0:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        else:
+            self.__position = value
 
     def area(self):
         """
@@ -128,4 +129,4 @@ class Square:
             print('\n' * self.__position[1], end='')
             print("\n".join([' ' * self.__position[0] +
                             '#' * self.__size
-                            for rows in range(self.__size)]))
+                             for rows in range(self.__size)]))
