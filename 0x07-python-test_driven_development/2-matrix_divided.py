bb4d3b5b465405a76df12e5e4426272c20c78496
jcgonzalezb <jcgonzalezb@gmail.com>
2021-10-04 22:50:28 -0500

    matrix_divided updated

---
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+This project module contains one method that divides all
+elements of a matrix. The matrix must be a list of lists
+of integers or floats. Each row of the matrix must be of
+the same size.
+"""
+def matrix_divided(matrix, div):
+    """This is a method that divides all elements of a 
+    matrix. div must be a number (integer or float).
+    div can’t be equal to 0. All elements of the matrix
+    should be divided by div, rounded to 2 decimal places.
+    Returns:
+        A new matrix.
+    """
+
