2617762b657496ae3a16c2571e25ab50a275f6de
jcgonzalezb <jcgonzalezb@gmail.com>
2021-10-06 11:42:26 -0500

    max interger test files

---
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Unittest for max_integer([..])
+"""
+import unittest
+max_integer = __import__('6-max_integer').max_integer
+
+class TestMaxInteger(unittest.TestCase):
+    """This class contains several methods to test the
+    6-max_integer.py file.
+    """
+    def test_00_max_integer(self):
+            """This is a method that XXXXXXXX
+            """
+            with self
+
+
+
