9945148c2b9d8fe05f8614ac221bf02e20e17dff
jcgonzalezb <jcgonzalezb@gmail.com>
2021-10-12 13:47:59 -0500

    from json string

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+This project module contains one method that returns
+the JSON representation of an object (string).
+"""
+
+
+import json
+
+
+def from_json_string(my_str):
+    """
+    This is a method that returns the JSON representation
+    of an object (string).
+    """
+    return json.dumps(my_obj)
