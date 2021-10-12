02015190183795ea96626b350620cd9240a2d278
jcgonzalezb <jcgonzalezb@gmail.com>
2021-10-12 13:37:27 -0500

    to json string files

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+This project module contains one method that returns
+the JSON representation of an object (string).
+"""
+
+
+import json
+def to_json_string(my_obj):
+    """
+    This is a method that returns the JSON representation
+    of an object (string).
+    """
+    return json.dumps(my_obj)
