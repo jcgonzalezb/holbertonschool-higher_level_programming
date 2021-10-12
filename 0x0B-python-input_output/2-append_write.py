ee210204394f116c22b85ded311ef88eaec900c8
jcgonzalezb <jcgonzalezb@gmail.com>
2021-10-12 11:24:41 -0500

    append write files

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+This project module contains one method that appends
+a string at the end of a text file (UTF8) and returns
+the number of characters added.
+"""
+
+
+def append_write(filename="", text=""):
+    """
+    This is a method that writes a string to a text
+    file (UTF8).
+    Returns:
+        The number of characters added.
+    """
+    with open(filename, 'a', encoding='utf8') as f:
+        f.writelines(''.join(text))
+     
+    return len(text)
