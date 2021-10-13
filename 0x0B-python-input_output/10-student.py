a4f2bc9d73f30c7dc558c9dc84f20784512a11e9
jcgonzalezb <jcgonzalezb@gmail.com>
2021-10-12 23:40:00 -0500

    Student to JSON with filter files

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""
+Write a class Student.
+Public instance attributes: first_name, last_name and age.
+"""
+
+
+class Student:
+    """
+    class Student in which def to_json(self) is
+    defined as Public instance method.
+    Public instance attributes:
+        first_name, last_name and age.
+    Methods:
+        def __init__(self, first_name, last_name, age):
+        def to_json(self, attrs=None)
+    """
+    def __init__(self, first_name, last_name, age):
+        """
+        Initialization function.
+        Attributes:
+            first_name: First name of the student.
+            last_name: Last name of the student.
+            age: Age of the student.
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        This is a method that returns the dictionary description
+        with simple data structure (list, dictionary, string,
+        integer and boolean) for JSON serialization of an object.
+        """
+        if attrs and all(isinstance(s, str) for s in attrs):
+            dic = {}
+            for att in attrs:
+                if att in self.__dict__.keys():
+                    dic[att] = self.__dict__[att]
+            return dic
+        else:
+            return self.__dict__
