cc93cb659868fbe7fa9bb6fad3c283da09a305ff
jcgonzalezb <jcgonzalezb@gmail.com>
2021-10-13 21:59:49 -0500

    square # 2 files

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""
+Write a class Square that inherits from Rectangle
+"""
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """inherits from Rectangle
+    Methods:
+        __init__(self, size):
+        area(self)
+    """
+    def __init__(self, size):
+        """
+        Initialization function.
+        Attribute is private.
+        Attribute:
+            size(int): Size of the side of a square.
+        """
+        super().integer_validator("size", size)
+        self.__size = size
+
+    def area(self):
+        """
+        Extends parent's empty method and returns area
+        """
+        return self.__size * self.__size
+
+    def __str__(self):
+        """
+        prints [Square] <width>/<height>
+        """
+        return "[{:s}] {:d}/{:d}".format(self.__class__.__name__,
+                                         self.__size, self.__size)
