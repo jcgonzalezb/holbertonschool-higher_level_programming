7129b4a267982bc27e2d2097f1226494341e9145
jcgonzalezb <jcgonzalezb@gmail.com>
2021-10-13 21:54:42 -0500

    square #1 files

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+Write a class Square that inherits from Rectangle
+"""
+
+
+Rectangle = __import__('8-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """inherits from Rectangle
+    Methods:
+        __init__(self, size):
+        area(self)
+    """
+    def __init__(self, size):
+        """
+        Initialization function.
+        Attribute is private.
+        Attribute:
+            size(int): Size of the side of a square.
+        """
+        super().integer_validator("size", size)
+        self.__size = size
+
+    def area(self):
+        """
+        Extends parent's empty method and returns area
+        """
+        return self.__size * self.__size
