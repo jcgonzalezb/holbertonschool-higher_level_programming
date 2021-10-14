68764e6fcdc558a9b50cb8f17422371eb5e29b53
jcgonzalezb <jcgonzalezb@gmail.com>
2021-10-13 23:08:16 -0500

    pascal triangle files

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+This project module contains one method that returns a list
+of lists of integers representing the Pascal’s triangle of n.
+"""
+
+
+def pascal_triangle(n):
+    """
+    This is a method that returns a list of lists of integers
+    representing the Pascal’s triangle of n.
+    """
+    index = 0
+    
+    while index < n:
+        element = myList[index]
+        #statement(s)
+        index += 1
+
+        return myList
