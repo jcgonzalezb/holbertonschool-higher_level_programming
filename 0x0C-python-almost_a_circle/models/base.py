06ac153efb0095e3d1f4e5b4bb7faa45328325af
jcgonzalezb <jcgonzalezb@gmail.com>
2021-10-16 22:53:35 -0500

    base file updated

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -8,21 +8,37 @@ class Base:
     """
     class Base in which nb_objects is defined
     as Private class attribute.
+    Methods:
+        def to_json_string(list_dictionaries)
     """
     __nb_objects = 0
 
     def __init__(self, id=None):
         """
         Initialization function.
-        Attributes:
-        id: Number of identification.
         If id is not None, assign the public instance
         attribute id with this argument value. Otherwise,
         increment __nb_objects and assign the new value
         to the public instance attribute id.
+        Attributes:
+            id: Number of identification.
         """
         if id is None:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
         else:
             self.id = id
+
+    def to_json_string(list_dictionaries):
+        """
+        Returns the JSON string representation of list_dictionaries.
+
+        If list_dictionaries is None or empty, return the string: "[]".
+        Otherwise, return the JSON string representation of list_dictionaries.
+        Attributes:
+            list_dictionaries: A list of dictionaries
+        """
+        if list_dictionaries is None or list_dictionaries == 0:
+            return "[]"
+        else:
+            return list_dictionaries
