b03c611055edabfe8187c7cac32494a94973212c
jcgonzalezb <jcgonzalezb@gmail.com>
2021-10-18 16:50:03 -0500

    base updated

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -47,7 +47,8 @@ class Base:
             list_dictionaries: A list of dictionaries
         """
         if list_dictionaries is None or len(list_dictionaries) == 0:
-            list_dictionaries = "[]"
+            list_dictionaries = []
+            return list_dictionaries
         else:
             return json.dumps(list_dictionaries)
 
