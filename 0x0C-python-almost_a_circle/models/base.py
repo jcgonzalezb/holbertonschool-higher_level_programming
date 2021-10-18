7e66d898867a6b6144b4f221ebaf3659e17b5ca8
jcgonzalezb <jcgonzalezb@gmail.com>
2021-10-18 17:32:44 -0500

    base updated

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -82,10 +82,9 @@ class Base:
         Attributes:
             json_string: String representing a list of dictionaries.
         """
-        if json_string is None or len(json_string) == 0:
+        if json_string is None or json_string == []:
             json_string = "[]"
-        else:
-            return json.loads(json_string)
+        return json.loads(json_string)
 
     @classmethod
     def create(cls, **dictionary):
