97987fdcf0ee52e11491a0e56560e952f2198180
jcgonzalezb <jcgonzalezb@gmail.com>
2021-10-18 20:43:56 -0500

    test_base revised

--- 0x0C-python-almost_a_circle/tests/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_base.py
@@ -7,49 +7,38 @@ from models import base
 from models import rectangle
 Rectangle = rectangle.Rectangle
 Base = base.Base
+Square = square.Square
 
 
-class Testbase(unittest.TestCase):
+class TestBase(unittest.TestCase):
     """This class contains several methods to test the
     base.py file.
     """
 
-    def test_00_case_width_success(self):
-        new_obj = Rectangle(5, 10)
-        self.assertEqual(new_obj.width, 5)
+    def test_class(self):
+        """Test class created is indeed Base"""
+        self.assertTrue(Base(100), self.__class__ == Base)
+    
+    def test_id_given(self):
+        """Test ids match when given"""
+        self.assertTrue(Base(77), self.id == 77)
+        self.assertTrue(Base(0), self.id == 0)
+        self.assertTrue(Base(1), self.id == 1)
+        self.assertTrue(Base(-10), self.id == -10)
 
-    def test_00_case_height_success(self):
-        new_obj = Rectangle(5, 10)
-        self.assertEqual(new_obj.height, 10)
+    def test_id_not_given(self):
+        """Test ids match incremented nb_objects when not given"""
+        self.assertTrue(Base(), self.id == 1)
+        self.assertTrue(Base(), self.id == 2)
+    
+    def test_private_attr_access(self):
+        """Test private attr are not accessible"""
+        with self.assertRaises(AttributeError):
+            print(Base.__nb_objects)
+            print(Base.nb_objects)
 
-    def test_00_case_x_default(self):
-        new_obj = Rectangle(5, 10)
-        self.assertEqual(new_obj.x, 0)
+    def test_invalid_args(self):
+        """Test too many args given throws error"""
+        with self.assertRaises(TypeError):
+            Base(50, 50)
 
-    def test_00_case_y_default(self):
-        new_obj = Rectangle(5, 10)
-        self.assertEqual(new_obj.y, 0)
-
-    def test_00_case_id_default(self):
-        new_obj = Rectangle(5, 10)
-        self.assertEqual(new_obj.id, 2)
-
-    def test_00_case_width_success_01(self):
-        new_obj = Rectangle(8, 12, 5, 3, 26)
-        self.assertEqual(new_obj.width, 8)
-
-    def test_00_case_height_success_01(self):
-        new_obj = Rectangle(8, 12, 5, 3, 26)
-        self.assertEqual(new_obj.height, 12)
-
-    def test_00_case_x_own(self):
-        new_obj = Rectangle(8, 12, 5, 3, 26)
-        self.assertEqual(new_obj.x, 5)
-
-    def test_00_case_y_own(self):
-        new_obj = Rectangle(8, 12, 5, 3, 26)
-        self.assertEqual(new_obj.y, 3)
-
-    def test_00_case_id_own(self):
-        new_obj = Rectangle(8, 12, 5, 3, 26)
-        self.assertEqual(new_obj.id, 26)
