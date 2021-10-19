566ffb9f013a1470475b5ca3b87007055936ea58
jcgonzalezb <jcgonzalezb@gmail.com>
2021-10-18 19:24:25 -0500

    test_base

--- 0x0C-python-almost_a_circle/tests/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_base.py
@@ -8,6 +8,7 @@ from models import rectangle
 Rectangle = rectangle.Rectangle
 Base = base.Base
 
+
 class Testbase(unittest.TestCase):
     """This class contains several methods to test the
     base.py file.
@@ -16,30 +17,39 @@ class Testbase(unittest.TestCase):
     def test_00_case_width_success(self):
         new_obj = Rectangle(5, 10)
         self.assertEqual(new_obj.width, 5)
+
     def test_00_case_height_success(self):
         new_obj = Rectangle(5, 10)
         self.assertEqual(new_obj.height, 10)
+
     def test_00_case_x_default(self):
         new_obj = Rectangle(5, 10)
         self.assertEqual(new_obj.x, 0)
+
     def test_00_case_y_default(self):
         new_obj = Rectangle(5, 10)
         self.assertEqual(new_obj.y, 0)
+
     def test_00_case_id_default(self):
         new_obj = Rectangle(5, 10)
         self.assertEqual(new_obj.id, 2)
+
     def test_00_case_width_success_01(self):
         new_obj = Rectangle(8, 12, 5, 3, 26)
         self.assertEqual(new_obj.width, 8)
+
     def test_00_case_height_success_01(self):
         new_obj = Rectangle(8, 12, 5, 3, 26)
         self.assertEqual(new_obj.height, 12)
+
     def test_00_case_x_own(self):
         new_obj = Rectangle(8, 12, 5, 3, 26)
         self.assertEqual(new_obj.x, 5)
+
     def test_00_case_y_own(self):
         new_obj = Rectangle(8, 12, 5, 3, 26)
         self.assertEqual(new_obj.y, 3)
+
     def test_00_case_id_own(self):
         new_obj = Rectangle(8, 12, 5, 3, 26)
         self.assertEqual(new_obj.id, 26)
