c99862d41ab909019877e85e072fc6e4a6f48032
jcgonzalezb <jcgonzalez@gmail.com>
2021-11-24 15:46:14 -0500

    auto generated added

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,9 @@
+-- Creates the the database hbtn_0d_usa and the table states.
+-- description: id INT unique, auto generated, can’t be null and is a primary key
+-- name VARCHAR(256) can’t be null.
+
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS states (
+	id INT UNIQUE IDENTITY(1,1) NOT NULL PRIMARY KEY,
+	name VARCHAR(256) NOT NULL
+);
