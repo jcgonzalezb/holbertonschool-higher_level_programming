dadf0a1b960c54bc5bc10b6a8727a3797d056e24
jcgonzalezb <jcgonzalez@gmail.com>
2021-11-23 21:29:33 -0500

    USE command removed

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,8 +1,6 @@
 -- Creates a table called first_table in the current database.
 -- Description: id INT, name VARCHAR(256).
 
-USE hbtn_0c_0;
-
 CREATE TABLE IF NOT EXISTS first_table (
 	id INT,
 	name VARCHAR(256)
