46c48b66130eb654dd7d0e90c7597baef9557af5
jcgonzalezb <jcgonzalez@gmail.com>
2021-11-23 23:20:05 -0500

    USE command

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,6 +1,8 @@
 -- Creates a table second_table in the database hbtn_0c_0
 -- and add multiples rows.
 
+USE hbtn_0c_0;
+
 CREATE TABLE IF NOT EXISTS second_table (id INT, name VARCHAR(256), score INT);
 INSERT INTO second_table (id, name, score) VALUES ('1', 'Jhon', '10');
 INSERT INTO second_table (id, name, score) VALUES ('2', 'Alex', '3');
