63a7fb5d1f97682ae1db577ea7112e92e7a8bdef
jcgonzalezb <jcgonzalez@gmail.com>
2021-11-23 21:44:08 -0500

    columns selected

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,6 +1,7 @@
 -- Lists all records of the table second_table
 -- of the database hbtn_0c_0.
 
-SELECT score, name
+SELECT id, name
 FROM second_table
-ORDER BY score DESC
+WHERE name IS NOT NULL
+ORDER BY score DESC;
