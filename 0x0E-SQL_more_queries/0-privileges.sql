e309b44e3b66a6dccb42d9b664cdbed9bbb46de7
jcgonzalezb <jcgonzalez@gmail.com>
2021-11-24 14:14:21 -0500

    initial version

---
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -0,0 +1,7 @@
+-- Creates a table called first_table in the current database.
+-- Description: id INT, name VARCHAR(256).
+
+CREATE TABLE IF NOT EXISTS first_table (
+	id INT,
+	name VARCHAR(256)
+);
