dcef0527a297de31c1120eb1f37d124d0b355da3
jcgonzalezb <jcgonzalez@gmail.com>
2021-11-23 23:40:58 -0500

    DESC command used

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,10 +1,4 @@
 -- Prints the full description of the table first_table
 -- from the database hbtn_0c_0 in your MySQL.
 
-USE information_schema;
-
-SELECT TABLE_NAME 'Table', COLUMN_NAME 'Field', COLUMN_TYPE 'Type', IS_NULLABLE 'Null',
-	COLUMN_KEY 'Key', COLUMN_DEFAULT 'Default', EXTRA 'Extra'
-FROM information_schema.columns
-WHERE table_schema = 'hbtn_0c_0'
-ORDER BY TABLE_NAME;
+DESC first_table;
