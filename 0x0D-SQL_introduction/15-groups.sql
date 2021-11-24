f1b3598050628256903e7089743a43dc35079b14
jcgonzalezb <jcgonzalez@gmail.com>
2021-11-23 20:54:04 -0500

    Alias added

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,8 +1,7 @@
 -- Lists the number of records with the same score
 -- in the table second_table of the database hbtn_0c_0.
 
-SELECT COUNT(*) as number, score
+SELECT score, COUNT (score) AS number
 FROM second_table
 GROUP BY score
-HAVING number > 1
-ORDER BY number DESC;
+HAVING COUNT (score) > 1;
