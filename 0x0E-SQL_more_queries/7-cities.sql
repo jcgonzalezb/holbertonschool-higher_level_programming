08e4adcd61cd0a57d33a4ae045f5281c6e6a0462
jcgonzalezb <jcgonzalez@gmail.com>
2021-11-24 18:37:26 -0500

    foreign hey set

---
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -0,0 +1,12 @@
+-- Creates the the database hbtn_0d_usa and the table cities.
+-- description: id INT unique, auto generated, can’t be null and is a primary key
+-- state_id INT, can’t be null and is a foreign key that references to id of the states table
+-- name VARCHAR(256) can’t be null.
+
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS cities (
+	id INT IDENTITY(1,1) PRIMARY KEY NOT NULL,
+	state_id INT IDENTITY(1,1) PRIMARY KEY NOT NULL,
+	name VARCHAR(256) NOT NULL
+	FOREIGN KEY (id) REFERENCES states (id)
+);
