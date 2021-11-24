9af6c4cecb06d8d7b22be4e849bbf38cab473ca0
jcgonzalezb <jcgonzalez@gmail.com>
2021-11-24 15:29:23 -0500

    UNIQUE constraint added

---
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -0,0 +1,7 @@
+-- Creates the table unique_id on your MySQL server.
+-- description: id INT with the default value 1, name VARCHAR(256).
+
+CREATE TABLE IF NOT EXISTS unique_id (
+	id INT DEFAULT 1 UNIQUE,
+	name VARCHAR(256)
+);
