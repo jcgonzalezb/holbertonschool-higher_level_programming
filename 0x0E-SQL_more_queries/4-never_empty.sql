698fca13f1ebd7a6bc658ab21ea8970b13236319
jcgonzalezb <jcgonzalez@gmail.com>
2021-11-24 15:25:56 -0500

    Default value

---
+++ 0x0E-SQL_more_queries/4-never_empty.sql
@@ -0,0 +1,7 @@
+-- Creates the table id_not_null on your MySQL server.
+-- description: id INT with the default value 1, name VARCHAR(256).
+
+CREATE TABLE IF NOT EXISTS force_name (
+	id INT DEFAULT 1,
+	name VARCHAR(256)
+);
