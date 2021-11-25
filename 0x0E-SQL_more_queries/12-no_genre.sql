8d0fb97609d15ff1daaf7b020f42803d8e299496
jcgonzalezb <jcgonzalez@gmail.com>
2021-11-25 15:49:12 -0500

    WHERE added

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -4,7 +4,8 @@
 -- Ascending order by tv_shows.title and tv_show_genres.genre_id
 
 SELECT tv_shows.title, tv_show_genres.genre_id
-FROM tv_show_genres
-FULL JOIN tv_shows
+FROM tv_shows
+FULL JOIN tv_show_genres
 ON tv_show_genres.show_id = tv_shows.id
+WHERE tv_show_genres.genre_id IS NULL
 ORDER BY tv_shows.title, tv_show_genres.genre_id ASC;
