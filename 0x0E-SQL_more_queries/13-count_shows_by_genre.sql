6ca6f480f21a73be25f3cbcc9aed91c63758c204
jcgonzalezb <jcgonzalez@gmail.com>
2021-11-25 17:25:42 -0500

    JOIN fixed

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -6,11 +6,8 @@
 -- Results must be sorted in descending order by the number of shows linked.
 
 SELECT tv_genres.name AS genre, COUNT (*) AS 'number_of_shows'
-FROM tv_shows
-JOIN tv_show_genres
-ON tv_shows.id = tv_show_genres.show_id
-JOIN tv_genres
-ON tv_genres.id = tv_show_genres.genre_id
-WHERE tv_genres.name IS NOT NULL
+FROM tv_genres
+INNER JOIN tv_show_genres
+ON tv_show_genres.genre_id = tv_genres.id
 GROUP BY genre
 ORDER BY number_of_shows DESC;
