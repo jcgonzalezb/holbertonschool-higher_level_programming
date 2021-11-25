26e9abe2f45a9c9a0b317a50484ddcb843e94933
jcgonzalezb <jcgonzalez@gmail.com>
2021-11-25 18:02:12 -0500

    WHERE fixed

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -6,7 +6,9 @@
 
 SELECT tv_genres.name
 FROM tv_genres
-RIGHT JOIN tv_show_genres
+JOIN tv_show_genres
 ON tv_show_genres.genre_id = tv_genres.id
-WHERE tv_show_genres.show_id = 8
+JOIN tv_shows
+ON tv_show_genres.show_id = tv_shows.id
+WHERE tvshows.title = Dexter
 ORDER BY tv_genres ASC;
