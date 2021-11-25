9761e80fb291b0ece5b15885599a23964fbd1d4f
jcgonzalezb <jcgonzalez@gmail.com>
2021-11-25 11:10:00 -0500

    ORDER BY fixed

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -6,4 +6,4 @@
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows, tv_show_genres
 WHERE tv_show_genres.genre_id >= 1
-ORDER BY tv_shows.title, tv_show_genres.genre_id ASC;
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
