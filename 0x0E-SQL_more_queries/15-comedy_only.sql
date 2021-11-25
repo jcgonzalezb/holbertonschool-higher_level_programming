1766fc6bad68bb0fc8b741a6411c64ea868775ee
jcgonzalezb <jcgonzalez@gmail.com>
2021-11-25 18:30:08 -0500

    Format fixed

--- 0x0E-SQL_more_queries/15-comedy_only.sql
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -9,6 +9,6 @@
 INNER JOIN tv_show_genres
         ON tv_show_genres.show_id = tv_shows.id
 INNER JOIN tv_genres
-        ON tv_show_genres.genres_id = tv_genres.id
+        ON tv_show_genres.genre_id = tv_genres.id
      WHERE tv_genres.name = "Comedy"
   ORDER BY tv_shows.title ASC;
