e857e7bb44f2921efe56cafec09539ed38a15a50
jcgonzalezb <jcgonzalez@gmail.com>
2021-11-25 15:26:58 -0500

    Correct JOIN applied

---
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -0,0 +1,10 @@
+-- Lists all shows contained in hbtn_0d_tvshows without a genre linked
+-- Display: tv_shows.title - tv_show_genres.genre_id.
+-- If a show doesn’t have a genre, display NULL.
+-- Ascending order by tv_shows.title and tv_show_genres.genre_id
+
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
+INNER JOIN tv_show_genres
+ON tv_show_genres.show_id = tv_shows.id
+ORDER BY tv_shows.title, tv_show_genres.genre_id ASC;
