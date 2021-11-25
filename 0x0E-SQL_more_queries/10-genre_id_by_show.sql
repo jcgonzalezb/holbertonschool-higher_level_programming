4425744f802e51914c574bedfbd724c93ca18255
jcgonzalezb <jcgonzalez@gmail.com>
2021-11-25 11:16:13 -0500

    WHERE corrected

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -5,5 +5,5 @@
 
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows, tv_show_genres
-WHERE tv_show_genres.genre_id >= 1
+WHERE tv_show_genres.genre_id>= 1
 ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
