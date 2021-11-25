4748ef2a5630d3ce5c065dd90be4511372638d15
jcgonzalezb <jcgonzalez@gmail.com>
2021-11-25 18:06:44 -0500

    ORDER BY fixed

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -11,4 +11,4 @@ ON tv_show_genres.genre_id = tv_genres.id
 INNER JOIN tv_shows
 ON tv_show_genres.show_id = tv_shows.id
 WHERE tv_shows.title = "Dexter"
-ORDER BY tv_genres ASC;
+ORDER BY tv_genres.name ASC;
