703933e5bb2245278822a3b1680984653653c5fd
jcgonzalezb <jcgonzalez@gmail.com>
2021-11-25 12:25:01 -0500

    SELECT fixed

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -3,7 +3,7 @@
 -- If a show doesn’t have a genre, display NULL.
 -- Ascending order by tv_shows.title and tv_show_genres.genre_id
 
-SELECT tv_shows.title, tv_show_genres.genre_id, NULLIF(tv_show_genres.genre_id, '')
+SELECT tv_shows.title, NULLIF(tv_show_genres.genre_id, '')
 	FROM tv_shows, tv_show_genres
 	WHERE tv_show_genres.show_id = tv_shows.id AND tv_show_genres.genre_id>= 1
 	ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
