77dfe7b081aef412e7d6572c6edf815fdcf259e6
jcgonzalezb <jcgonzalez@gmail.com>
2021-11-25 15:31:07 -0500

    Correct JOIN applied

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -5,6 +5,6 @@
 
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_show_genres
-Right JOIN tv_shows
+FULL JOIN tv_shows
 ON tv_show_genres.show_id = tv_shows.id
 ORDER BY tv_shows.title, tv_show_genres.genre_id ASC;
