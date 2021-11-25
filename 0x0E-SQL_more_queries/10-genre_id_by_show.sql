66a39f885f9ca5a539361bbea30d729d9a4b9cd3
jcgonzalezb <jcgonzalez@gmail.com>
2021-11-25 10:55:47 -0500

    FROM tables fixed

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -4,6 +4,6 @@
 -- Ascending order by tv_shows.title and tv_show_genres.genre_id
 
 SELECT tv_shows.title, tv_show_genres.genre_id
-FROM cities, states
+FROM tv_shows, tv_show_genres
 WHERE tv_show_genres.genre_id >= 1
 ORDER BY tv_shows.title, tv_show_genres.genre_id ASC;
