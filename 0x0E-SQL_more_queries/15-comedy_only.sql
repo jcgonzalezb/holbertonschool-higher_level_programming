465306ecaa50759ab75ff05ddb21826873752503
jcgonzalezb <jcgonzalez@gmail.com>
2021-11-25 18:27:40 -0500

    Format fixed

--- 0x0E-SQL_more_queries/15-comedy_only.sql
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -4,11 +4,11 @@
 -- Each record should display: tv_shows.title.
 -- Results must be sorted in ascending order by the show title.
 
-SELECT tv_shows.title
-FROM tv_shows
+    SELECT tv_shows.title
+      FROM tv_shows
 INNER JOIN tv_show_genres
-ON tv_show_genres.genre_id = tv_shows.id
+        ON tv_show_genres.show_id = tv_shows.id
 INNER JOIN tv_genres
-ON tv_show_genres.show_id = tv_genres.id
-WHERE tv_genres.name = "Comedy"
-ORDER BY tv_shows.title ASC;
+        ON tv_show_genres.genres.id = tv_genres.id
+     WHERE tv_genres.name = "Comedy"
+  ORDER BY tv_shows.title ASC;
