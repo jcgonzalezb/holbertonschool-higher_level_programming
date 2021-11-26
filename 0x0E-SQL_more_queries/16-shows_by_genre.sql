9a994b528b17ed52fd41780a026597b8cf2687bb
jcgonzalezb <jcgonzalez@gmail.com>
2021-11-26 00:22:41 -0500

    Initial version

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -4,11 +4,11 @@
 -- Each record should display: tv_shows.title - tv_genres.name
 -- Results must be sorted in ascending order by the show title and genre name
 
-    SELECT tv_shows.title
+    SELECT tv_shows.title, tv_genres.name
       FROM tv_shows
 INNER JOIN tv_show_genres
         ON tv_show_genres.show_id = tv_shows.id
 INNER JOIN tv_genres
         ON tv_show_genres.genre_id = tv_genres.id
      WHERE tv_genres.name = "Comedy"
-  ORDER BY tv_shows.title ASC;
+  ORDER BY tv_shows.title ASC AND tv_genres.name ASC;
