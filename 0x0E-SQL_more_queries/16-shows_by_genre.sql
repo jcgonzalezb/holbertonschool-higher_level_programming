506e1e998e2e717e0f52904d216f31b1bbbc6246
jcgonzalezb <jcgonzalez@gmail.com>
2021-11-27 17:26:13 -0500

    ORDER fixed

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -10,4 +10,4 @@ LEFT JOIN tv_show_genres
        ON tv_show_genres.show_id = tv_shows.id
 LEFT JOIN tv_genres
        ON tv_show_genres.genre_id = tv_genres.id
- ORDER BY tv_shows.title ASC AND tv_genres.name ASC;
+ ORDER BY tv_shows.title, AND tv_genres.name ASC;
