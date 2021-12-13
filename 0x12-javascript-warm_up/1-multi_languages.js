e871df34a65113ebca80cefe8610c1c86ef7811b
jcgonzalezb <jcgonzalez@gmail.com>
2021-12-13 12:06:40 -0500

    comments fixed

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,4 +1,6 @@
 #!/usr/bin/node
-// Write a script that prints “JavaScript is amazing”
+// Write a script that prints: The first line: “C is fun”
+// The second line: “Python is cool”
+// The third line: “JavaScript is amazing”
 const myVar = 'C is fun\nPython is cool\nJavaScript is amazing';
 console.log(myVar);
