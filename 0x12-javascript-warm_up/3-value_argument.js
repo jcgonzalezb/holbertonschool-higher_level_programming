6328ad4b54a48d605101be209fae98d8b8c97bdf
jcgonzalezb <jcgonzalez@gmail.com>
2021-12-13 17:02:11 -0500

    spaces fixed

---
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+// Write a script that prints the first argument passed to it.
+
+if (process.argv.length <= 2) {
+  console.log('No argument');
+} else {
+  for (let i = 2; i < process.argv.length; ++i) {
+    console.log(`${process.argv[i]}`);
+  }
+}
