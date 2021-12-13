525222d4505ca8c9e3def21d06fa5772ba2dc727
jcgonzalezb <jcgonzalez@gmail.com>
2021-12-13 17:44:42 -0500

    if fixed

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 // Write a script that prints the first argument passed to it.
 
-if (process.argv.length <= 2) {
+if (process.argv[2] === undefined) {
   console.log('No argument');
 } else {
   for (let i = 2; i < 3; ++i) {
