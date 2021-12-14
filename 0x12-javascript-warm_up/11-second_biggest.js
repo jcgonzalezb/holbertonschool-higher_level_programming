8900914de8e2f8329cbe3b9905e1aa29b5231a84
jcgonzalezb <jcgonzalez@gmail.com>
2021-12-13 23:52:12 -0500

    fix array else

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,33 +1,14 @@
 #!/usr/bin/node
 // Write a script that searches the second biggest integer in the list of arguments.
 
-const arr = process.argv;
-const n = process.argv.length;
-
-function print2largest (arr, n) {
-  let i;
-  let largest = -2454635434;
-  let second = -2454635434;
-
-  if (n < 2) {
-    console.log(0);
-    return;
-  }
-
-  for (i = 0; i < n; i++) {
-    if (arr[i] > largest) {
-      second = largest;
-      largest = arr[i];
-    } else if (arr[i] !== largest && arr[i] > second) {
-      second = arr[i];
-    }
-  }
-
-  if (second === -2454635434) {
-    console.log(0);
+const numsArray = process.argv.slice(2);
+function secondMax (array) {
+  if (array.length < 2) {
+    return (0);
   } else {
-    console.log(second);
+    array.sort((x, y) => x - y);
+    array.pop();
+    return (array.pop());
   }
 }
-
-print2largest(arr, n);
+console.log(secondMax(numsArray));
