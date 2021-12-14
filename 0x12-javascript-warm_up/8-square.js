8602037629ed9342a3e8fe8299de8b586d85d585
jcgonzalezb <jcgonzalez@gmail.com>
2021-12-13 21:48:46 -0500

    for cycle fixed

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,7 +1,6 @@
 #!/usr/bin/node
 // Write a script that prints x times “C is fun”.
 
-
 let i; let j = 0;
 
 if (process.argv[2] === undefined) {
@@ -9,11 +8,12 @@ if (process.argv[2] === undefined) {
 } else if (parseInt(process.argv[2])) {
   const size = parseInt(process.argv[2]);
   for (i = 0; i < size; i++) {
+    let temp = '';
     for (j = 0; j < size; j++) {
-      console.log('X');
+      temp = temp + '' + 'X';
     }
-    console.log('');
+    console.log(temp);
   }
-  } else {
-   console.log('Missing size');
+} else {
+  console.log('Missing size');
 }
