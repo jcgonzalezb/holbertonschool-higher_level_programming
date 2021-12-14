7d29510c17e235c99bda3a6b45f7ac89c1fbf695
jcgonzalezb <jcgonzalez@gmail.com>
2021-12-14 14:39:40 -0500

    width and height position fixed

---
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+// Write a class Rectangle that defines a rectangle.
+
+module.exports = class Rectangle {
+  constructor (width, height) {
+    this.width = width;
+    this.height = height;
+  }
+};
