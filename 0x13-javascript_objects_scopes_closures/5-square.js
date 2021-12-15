438685b6064f7ed8cb6bec574ead674e9b73d6a3
jcgonzalezb <jcgonzalez@gmail.com>
2021-12-14 22:46:39 -0500

    import class Rectangle fixed

--- 0x13-javascript_objects_scopes_closures/5-square.js
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -1,38 +1,11 @@
 #!/usr/bin/node
 // Write a class Square that defines a square and inherits from Rectangle.
 
-class Rectangle {
-  constructor (w, h) {
-    this.name = 'Rectangle';
-    if (w > 0 && h > 0) {
-      this.width = w;
-      this.height = h;
-    }
-  }
-}
+const Rectangle = require('./4-rectangle');
 
 module.exports = class Square extends Rectangle {
   constructor (size) {
     super(size, size);
     this.name = 'Square';
-    if (size > 0) {
-      this.size = size;
-    }
-  }
-
-  print () {
-    let i = 0;
-    let j = 0;
-    for (i = 0; i < this.size; i++) {
-      let temp = '';
-      for (j = 0; j < this.size; j++) {
-        temp = temp + '' + 'X';
-      }
-      console.log(temp);
-    }
-  }
-
-  double () {
-    this.size *= 2;
   }
 };
