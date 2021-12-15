65c15c63abf9e557aba4dbb0622e6b332ece542d
jcgonzalezb <jcgonzalez@gmail.com>
2021-12-14 21:07:57 -0500

    semistandard fixed

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -2,46 +2,46 @@
 // Write a class Rectangle that defines a rectangle.
 
 module.exports = class Rectangle {
- constructor(w, h) {
-  if (w > 0 && h > 0) {
-   this.width = w;
-   this.height = h;
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
   }
- }
 
- print() {
-  let i = 0;
-  let j = 0;
-  for (i = 0; i < this.height; i++) {
-   let temp = '';
-   for (j = 0; j < this.width; j++) {
-    temp = temp + '' + 'X';
-   }
-   console.log(temp);
+  print () {
+    let i = 0;
+    let j = 0;
+    for (i = 0; i < this.height; i++) {
+      let temp = '';
+      for (j = 0; j < this.width; j++) {
+        temp = temp + '' + 'X';
+      }
+      console.log(temp);
+    }
   }
- }
 
- rotate() {
-  let i = 0;
-  let j = 0;
-  for (i = 0; i < this.width; i++) {
-   let temp = '';
-   for (j = 0; j < this.height; j++) {
-    temp = temp + '' + 'X';
-   }
-   console.log(temp);
+  rotate () {
+    let i = 0;
+    let j = 0;
+    for (i = 0; i < this.width; i++) {
+      let temp = '';
+      for (j = 0; j < this.height; j++) {
+        temp = temp + '' + 'X';
+      }
+      console.log(temp);
+    }
   }
- }
 
- double() {
-  let i = 0;
-  let j = 0;
-  for (i = 0; i < this.height * 2; i++) {
-   let temp = '';
-   for (j = 0; j < this.width * 2; j++) {
-    temp = temp + '' + 'X';
-   }
-   console.log(temp);
+  double () {
+    let i = 0;
+    let j = 0;
+    for (i = 0; i < this.height * 2; i++) {
+      let temp = '';
+      for (j = 0; j < this.width * 2; j++) {
+        temp = temp + '' + 'X';
+      }
+      console.log(temp);
+    }
   }
- }
 };
