4f042e26ac183a67cb93f22286515324a8e43f3b
jcgonzalezb <jcgonzalez@gmail.com>
2021-12-14 22:38:39 -0500

    size assigment fixed

--- 0x13-javascript_objects_scopes_closures/5-square.js
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -1,35 +1,38 @@
 #!/usr/bin/node
-// Write a class Rectangle that defines a rectangle.
+// Write a class Square that defines a square and inherits from Rectangle.
 
-module.exports = class Rectangle {
-	constructor (w, h) {
-	  if (w > 0 && h > 0) {
-	    this.width = w;
-	    this.height = h;
-	  }
-	}
-      
-	print () {
-	  let i = 0;
-	  let j = 0;
-	  for (i = 0; i < this.height; i++) {
-	    let temp = '';
-	    for (j = 0; j < this.width; j++) {
-	      temp = temp + '' + 'X';
-	    }
-	    console.log(temp);
-	  }
-	}
-      
-	rotate () {
-	  const tmp = this.width;
-	  this.width = this.height;
-	  this.height = tmp;
-	}
-      
-	double () {
-	  this.width *= 2;
-	  this.height *= 2;
-	}
-      };
-      
+class Rectangle {
+  constructor (w, h) {
+    this.name = 'Rectangle';
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+}
+
+module.exports = class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+    this.name = 'Square';
+    if (size > 0) {
+      this.size = size;
+    }
+  }
+
+  print () {
+    let i = 0;
+    let j = 0;
+    for (i = 0; i < this.size; i++) {
+      let temp = '';
+      for (j = 0; j < this.size; j++) {
+        temp = temp + '' + 'X';
+      }
+      console.log(temp);
+    }
+  }
+
+  double () {
+    this.size *= 2;
+  }
+};
