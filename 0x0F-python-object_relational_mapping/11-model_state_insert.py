483791ffaf956baa985aaece5ecd6207a3b42aa8
jcgonzalezb <jcgonzalez@gmail.com>
2021-12-19 17:11:23 -0500

    print last id of the list fix

---
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+Write a script that prints the State object with the
+name passed as argument from the database.
+"""
+
+
+from sys import argv
+from sqlalchemy import create_engine, desc
+from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
+
+
+if __name__ == "__main__":
+
+    username = argv[1]
+    passwrd = argv[2]
+    database = argv[3]
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        username, passwrd, database), pool_pre_ping=True)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    myresult = session.query(State).order_by(desc(State.id)).first()
+
+    print('{:d}'.format(myresult.id))
+
+    session.close()
