9eab2f17b9589c9a93521ce90e8ae488fecd2ef3
jcgonzalezb <jcgonzalez@gmail.com>
2021-12-19 20:49:35 -0500

    relationship fixed

---
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+Definition of class City.
+"""
+
+from sqlalchemy.orm import declarative_base, relationship
+from sqlalchemy import Column, Integer, String, ForeignKey
+
+Base = declarative_base()
+
+
+class City(Base):
+    """
+    Class City. inherits from Base; instance of base
+    Creates a table "states" and it is linked to MySQL.
+    """
+    __tablename__ = "cities"
+
+    id = Column(Integer, nullable=False, primary_key=True)
+    name = Column(String(128), nullable=False)
+    state_id = relationship("States", back_populates="cities")
