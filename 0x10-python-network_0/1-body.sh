65c3601ddf28e958f1709ed54193e26eb1820e74
jcgonzalezb <jcgonzalez@gmail.com>
2022-01-08 17:58:18 -0500

    curl fixed

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-# Script that takes in a URL, sends a request to that URL, and displays the size of the body of the response.
-# Display only body of a 200 status code response.
+# Script that takes in a URL, sends a request to that URL, and displays only body of a 200 status code response.
 
-curl -sI "$1" -o test |cat test | sed 's/.*Content-Length: //' | cut -d " " -f 1 | sed -n '5p'
+curl -Ls "$1"
