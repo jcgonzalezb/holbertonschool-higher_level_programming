4055ca58f15ecdc05b91b07e5160aa2437bb225b
jcgonzalezb <jcgonzalez@gmail.com>
2022-01-08 00:04:55 -0500

    curl fixed

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Script that takes in a URL, sends a request to that URL, and displays the size of the body of the response.
 
-curl -sI 34.139.188.96 -o test |cat test | sed 's/.*Content-Length: //' | cut -d " " -f 1 | sed -n '5p'
+curl -sI "$1" -o test |cat test | sed 's/.*Content-Length: //' | cut -d " " -f 1 | sed -n '5p'
