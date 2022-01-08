ef42e146f3bf1832f0dab7166d9d0143bcd4038d
jcgonzalezb <jcgonzalez@gmail.com>
2022-01-08 17:31:20 -0500

    sed fixed

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Script that takes in a URL, sends a request to that URL, and displays the size of the body of the response.
 
-curl -sLI -- "$1" | grep -i 'content-length' | tr -s '\t ' ' ' | cut -d ' ' -f 2
+curl -sI "$1" -o test |cat test | sed 's/.*Content-Length: //' | cut -d " " -f 1 | sed -n '5p'
