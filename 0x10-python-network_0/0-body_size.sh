84cd378b28b485c9040346b55c8fe8671ad30424
jcgonzalezb <jcgonzalez@gmail.com>
2022-01-08 17:30:47 -0500

    cut fixed

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Script that takes in a URL, sends a request to that URL, and displays the size of the body of the response.
 
-curl -sI "$1" | grep 'Content-Length:' | cut -f2 -d' '
+curl -sLI -- "$1" | grep -i 'content-length' | tr -s '\t ' ' ' | cut -d ' ' -f 2
