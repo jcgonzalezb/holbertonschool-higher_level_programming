06aa045a8b8a6df7cf263581bc765553525c2ed1
jcgonzalezb <jcgonzalez@gmail.com>
2022-01-09 15:34:43 -0500

    arguments fixed

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,13 +1,25 @@
 #!/usr/bin/python3
 """
-Python script that takes in a URL, sends a request to the URL and displays
-the value of the X-Request-Id variable found in the header of the response.
+Python script that takes in a URL, and an email, sends a POST
+request to the passed URL with the email as a parameter, and
+displays the body of the response (decoded in utf-8).
 """
 import urllib.request
+import urllib.parse
 import sys
 
 if __name__ == "__main__":
 
-    argv = sys.argv[1]
-    with urllib.request.urlopen(argv) as response:
-        print(response.info()["X-Request-Id"])
+    URL = sys.argv[1]
+    email = sys.argv[2]
+    params = {
+        "email": email
+    }
+
+    query_string = urllib.parse.urlencode(params)
+    data = query_string.encode("ascii")
+
+    with urllib.request.urlopen(URL, data) as response:
+        response_text = response.read()
+        string = response_text.decode('utf-8')
+        print("Your email is:: {}".format(string))
