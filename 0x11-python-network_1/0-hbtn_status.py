92bacd6f0c76431519a89619f34630b92c9f1abd
jcgonzalezb <jcgonzalez@gmail.com>
2022-01-09 13:09:44 -0500

    tab fixed

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -6,7 +6,7 @@ if __name__ == "__main__":
     with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
         data = response.read()
         print("Body response:")
-        print("	- type: {}".format(type(data)))
-        print("	- content: {}".format(data))
+        print("\t- type: {}".format(type(data)))
+        print("\t- content: {}".format(data))
         html = data.decode('utf-8')
-        print("	- utf8 content: {}".format(html))
+        print("\t- utf8 content: {}".format(html))
