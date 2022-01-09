ce7f36095d4532bb6fd35396bc57f834735fc23b
jcgonzalezb <jcgonzalez@gmail.com>
2022-01-09 13:01:52 -0500

    decode order changed

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -5,8 +5,8 @@ import urllib.request
 if __name__ == "__main__":
     with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
         data = response.read()
-        html = data.decode('utf-8')
         print("Body response:")
         print("	- type: {}".format(type(data)))
         print("	- content: {}".format(response.peek()))
+        html = data.decode('utf-8')
         print("	- utf8 content: {}".format(html))
