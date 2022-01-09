2b3625bdf6fcb4a225b3496466e2869ca2574205
jcgonzalezb <jcgonzalez@gmail.com>
2022-01-08 22:06:28 -0500

    cut fixed

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Script that takes in a URL and displays all HTTP methods the server will accept.
-curl -sX OPTIONS "$1" -i | grep 'allow:' | cut -f2 -d' '
+curl -sX OPTIONS "$1" -i | grep 'Allow:' | cut -f2-10 -d' '
