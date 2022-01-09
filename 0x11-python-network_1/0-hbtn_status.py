9fc64cf3e6b58e733131d6e5420021bc30c9fda0
jcgonzalezb <jcgonzalez@gmail.com>
2022-01-09 13:17:14 -0500

    variable fixed

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -5,8 +5,8 @@ import urllib.request
 if __name__ == "__main__":
     with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
         data = response.read()
-        print("Body response:")
+        print('Body response:')
         print("\t- type: {}".format(type(data)))
         print("\t- content: {}".format(data))
-        html = data.decode('utf-8')
-        print("\t- utf8 content: {}".format(html))
+        string = data.decode('utf-8')
+        print("\t- utf8 content: {}".format(string))
