7791a557c1a3d4f38fcced3207f4d0bbe8bf3b96
jcgonzalezb <jcgonzalez@gmail.com>
2022-01-09 21:42:01 -0500

    error fixed

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+Python script that takes in a URL, sends a request to the URL and
+displays the body of the response (decoded in utf-8).
+"""
+import urllib.request
+import urllib.parse
+import urllib. error
+import sys
+
+if __name__ == "__main__":
+
+    URL = sys.argv[1]
+
+    query_string = urllib.parse.urlencode(params)
+    data = query_string.encode("ascii")
+
+    with urllib.request.urlopen(URL, data) as response:
+        try:
+            response_text = response.read()
+            string = response_text.decode('utf-8')
+            print(string)
+        except urllib.error.HTTPError as e:
+            print("Error code:", e.reason)
