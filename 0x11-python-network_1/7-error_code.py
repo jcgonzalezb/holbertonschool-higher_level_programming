049a6c3b5c161d92adea54c39d20bffba82fb158
jcgonzalezb <jcgonzalez@gmail.com>
2022-01-09 23:09:02 -0500

    conditinal fixed

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -13,7 +13,6 @@ if __name__ == "__main__":
     URL = sys.argv[1]
 
     response = requests.get(URL)
-    response.raise_for_status()
     if response.status_code >= 400:
         print("Error code: {}".format(response.status_code))
     else:
