1411a1e131169e33dd9719977ad046c8a8bcd723
jcgonzalezb <jcgonzalez@gmail.com>
2022-01-09 22:58:11 -0500

    error fixed

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,25 +1,20 @@
 #!/usr/bin/python3
 """
-Python script that takes in a URL and an email address,
-sends a POST request to the passed URL with the email as
-a parameter, and finally displays the body of the response.
+Python script  that takes in a URL, sends a request to the URL
+and displays the body of the response.
 """
 import requests
+from requests.exceptions import HTTPError
 import sys
+
 
 if __name__ == "__main__":
 
     URL = sys.argv[1]
-    email = sys.argv[2]
-    response_text = requests.post(URL, data={"email": email})
-    string = response_text.decode('utf-8')
-    print("Your email is: {}".format(string))
 
-
-    try:
-        with urllib.request.urlopen(URL) as response:
-            response_text = response.read()
-            string = response_text.decode('utf-8')
-            print(string)
-    except urllib.error.HTTPError as e:
-        print("Error code: {}".format(e.code))
+    for url in [URL]:
+        try:
+            response = requests.get(url)
+            response.raise_for_status()
+        except HTTPError as http_err:
+            print("Error code: {}".format(http_err))
