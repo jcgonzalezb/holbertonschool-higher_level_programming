a5cce91fb19c9fe08cabc005947cf31aec652bed
jcgonzalezb <jcgonzalez@gmail.com>
2022-01-10 22:57:41 -0500

    condition fixed

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -9,11 +9,20 @@ import sys
 
 if __name__ == "__main__":
 
-    URL = http: // 0.0.0.0: 5000/search_user
-    letter = sys.argv[1]
+    URL = 'http://0.0.0.0:5000/search_user'
 
-    response = requests.get(URL)
-    if response.status_code >= 400:
-        print("Error code: {}".format(response.status_code))
+    if len(sys.argv) < 2:
+        letter = ""
     else:
-        print(response.text)
+        letter = sys.argv[1]
+
+    response = requests.post(URL, data={"q": letter})
+
+    try:
+        r_json = response.json()
+        if r_json:
+            print("[{}] {}".format(r_json.get('id'), r_json.get('name')))
+        else:
+            print("No result")
+    except ValueError as e:
+        print("Not a valid JSON")
