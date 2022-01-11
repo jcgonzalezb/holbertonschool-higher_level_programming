e0a885a95fe12be12fde26e9e32de93409f2155a
jcgonzalezb <jcgonzalez@gmail.com>
2022-01-10 23:26:12 -0500

    requests fixed

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,28 +1,16 @@
 #!/usr/bin/python3
 """
-Python script that takes in a letter and sends a POST request to
-http://0.0.0.0:5000/search_user with the letter as a parameter.
+Python script that takes your GitHub credentials (username and password)
+and uses the GitHub API to display your id.
 """
 import requests
+from requests.auth import HTTPBasicAuth
 import sys
 
 
 if __name__ == "__main__":
 
-    URL = 'http://0.0.0.0:5000/search_user'
-
-    if len(sys.argv) < 2:
-        letter = ""
-    else:
-        letter = sys.argv[1]
-
-    response = requests.post(URL, data={"q": letter})
-
-    try:
-        r_json = response.json()
-        if r_json:
-            print("[{}] {}".format(r_json.get('id'), r_json.get('name')))
-        else:
-            print("No result")
-    except ValueError as e:
-        print("Not a valid JSON")
+    URL = 'https://api.github.com/user'
+    response = requests.get(URL, auth=HTTPBasicAuth(sys.argv[1], sys.argv[2]))
+    r_json = response.json()
+	print(r_json.get('id'))
