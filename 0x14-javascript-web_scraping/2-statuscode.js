0c2c1e19462c216dc23c37a68c303bc6c52f5b91
jcgonzalezb <jcgonzalez@gmail.com>
2022-02-01 18:18:24 -0500

    argmument URL fixed

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+// Write a script that display the status code of a GET request.
+
+const request = require('request');
+
+const url = process.argv[2];
+
+request(url, (error, response, body) => {
+  if (error) console.log(error);
+
+  console.log(`code: ${response.statusCode}`);
+});
