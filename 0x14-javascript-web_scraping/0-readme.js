35e9ce047a0e2a910ab8055c7934720acf2ff514
jcgonzalezb <jcgonzalez@gmail.com>
2022-02-01 15:13:26 -0500

    argument read fixed

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+// Write a script that reads and prints the content of a file.
+
+const fs = require('fs');
+
+fs.readFile(process.argv[2], 'utf-8', (err, data) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+
+  console.log(data);
+});
