2e99178eed1a6b21be3b52a5f165700a802c43d9
jcgonzalezb <jcgonzalez@gmail.com>
2022-02-01 15:44:43 -0500

    semistandard fixed

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -3,9 +3,9 @@
 
 const fs = require('fs');
 
-const data = process.argv[3]
+const data = process.argv[3];
 
-fs.writeFile(process.argv[2], "\ufeff" + data, (err) => {
+fs.writeFile(process.argv[2], '\ufeff' + data, (err) => {
   if (err) {
     console.error(err);
     return;
