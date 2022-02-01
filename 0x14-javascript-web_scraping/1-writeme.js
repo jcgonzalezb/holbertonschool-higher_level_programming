ddd6584ce169ad0144d6d42b933dfdb111559536
jcgonzalezb <jcgonzalez@gmail.com>
2022-02-01 15:43:52 -0500

    utf-8 fixed

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -5,7 +5,7 @@ const fs = require('fs');
 
 const data = process.argv[3]
 
-fs.writeFile(process.argv[2], data, (err) => {
+fs.writeFile(process.argv[2], "\ufeff" + data, (err) => {
   if (err) {
     console.error(err);
     return;
