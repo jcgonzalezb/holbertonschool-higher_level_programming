bdb9adcf35231cdc94269ca97489790bb30a154b
jcgonzalezb <jcgonzalez@gmail.com>
2022-02-01 23:09:03 -0500

    constants fixed

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -3,13 +3,17 @@
 
 const url = process.argv[2];
 const filename = process.argv[3];
-const fs = require('fs');
 const request = require('request');
 
-request(url, function (err, response, body) {
+request(url, (err, resp, body) => {
   if (err) {
     console.log(err);
   } else {
-    fs.writeFile(filename, body, 'utf8');
+    const fs = require('fs');
+    fs.writeFile(filename, body, 'utf-8', (err) => {
+      if (err) {
+        console.log(err);
+      }
+    });
   }
 });
