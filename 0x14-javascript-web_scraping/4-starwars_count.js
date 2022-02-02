dd7409276c2c1d083cd06cdbee4cc86e6b6f8b7d
jcgonzalezb <jcgonzalez@gmail.com>
2022-02-01 22:58:47 -0500

    for fixed

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,3 +1,25 @@
 #!/usr/bin/node
-// Write a script that prints the title of a Star Wars movie where the episode number matches a given integer.
+// Write a script that prints number of movies where the character “Wedge Antilles” (ID = 18) is present.
 
+const url = process.argv[2];
+const request = require('request');
+
+request(url, function (err, response, body) {
+  if (err) {
+    console.log(err);
+  } else if (response.statusCode === 200) {
+    const films = JSON.parse(body).results;
+    let count = 0;
+    for (const i in films) {
+      const chars = films[i].characters;
+      for (const c in chars) {
+        if (chars[c].includes('18')) {
+          count++;
+        }
+      }
+    }
+    console.log(count);
+  } else {
+    console.log('Erorr Code:' + response.statusCode);
+  }
+});
