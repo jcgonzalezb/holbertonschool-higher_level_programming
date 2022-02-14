935f76edd2e50f46a4f7e81ace4e948830f50a3c
jcgonzalezb <jcgonzalez@gmail.com>
2022-02-14 12:30:46 -0500

    File Permissions fixed

---
+++ 0x15-javascript-web_jquery/2-script.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+// Write a JavaScript script that updates the text color of the <header> element to red (#FF0000) when the user clicks on the tag DIV#red_header. Must use the JQuery API.
+
+$(document).ready(function () {
+	$("p").on({
+		mouseenter: function(){
+		    $(this).css("color", "red");
+		}
+	    });  
+      });
+      
