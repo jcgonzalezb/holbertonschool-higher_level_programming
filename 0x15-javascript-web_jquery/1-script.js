ab47a707f67c211a58862b191b095d3b38d9e5f3
jcgonzalezb <jcgonzalez@gmail.com>
2022-02-14 12:26:57 -0500

    semistandard fixed

---
+++ 0x15-javascript-web_jquery/1-script.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+// Write a JavaScript script that updates the text color of the <header> element to red (#FF0000).  Must use the JQuery API.
+
+$(document).ready(function () {
+  $('header').css({ color: 'red' });
+});
