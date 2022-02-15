d38119a44abb28b7758ce5dec759f71692295adf
jcgonzalezb <jcgonzalez@gmail.com>
2022-02-14 21:07:15 -0500

    text added

--- 0x15-javascript-web_jquery/7-script.js
+++ 0x15-javascript-web_jquery/7-script.js
@@ -1,9 +1,10 @@
 #!/usr/bin/node
-// Write a JavaScript script that fetches the character name from this URL: https://swapi-api.hbtn.io/api/people/5/?format=json.
-$(document).ready(function () {
-  $('div').on({
-    click: function () {
-      $('header').text('New Header!!!');
-    }
-  });
+// Write a JavaScript script that fetches the character name from a URL.
+
+$.ajax({
+  type: 'GET',
+  url: 'https://swapi-api.hbtn.io/api/people/5/?format=json',
+  success: function (data) {
+    $('div').text(data.name);
+  }
 });
