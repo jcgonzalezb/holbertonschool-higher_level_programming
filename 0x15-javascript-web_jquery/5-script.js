97d2cdc5ba0feddabcd8b2d6af2bd65e41b56091
jcgonzalezb <jcgonzalez@gmail.com>
2022-02-14 19:25:24 -0500

    initial version

---
+++ 0x15-javascript-web_jquery/5-script.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+// Write a JavaScript script that toggles the class of the <header> element when the user clicks on the tag DIV#toggle_header.
+$(document).ready(function () {
+  $('div').on({
+    click: function () {
+      $('header').toggleClass('green red');
+    }
+  });
+});
