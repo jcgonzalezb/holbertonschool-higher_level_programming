8ba9da95e3e6646e982acaf343c0dc1045540a87
jcgonzalezb <jcgonzalez@gmail.com>
2022-02-14 20:09:35 -0500

    initial version

---
+++ 0x15-javascript-web_jquery/7-script.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+// Write a JavaScript script that fetches the character name from this URL: https://swapi-api.hbtn.io/api/people/5/?format=json.
+$(document).ready(function () {
+  $('div').on({
+    click: function () {
+      $('header').text('New Header!!!');
+    }
+  });
+});
